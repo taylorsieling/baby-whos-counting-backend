5f0c39207c57f21f51079f29c18afb40ada3ac7c
taylorsieling <taylorasieling@gmail.com>
2021-07-02 17:21:11 -0500

    finalizing spotify authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    def issue_token(payload)
+        JWT.encode(payload, ENV["JWT_SECRET"])
+        byebug
+    end
+  
+    def decode(jwt_token)
+        JWT.decode(jwt_token, ENV["JWT_SECRET"])
+    end
 end
